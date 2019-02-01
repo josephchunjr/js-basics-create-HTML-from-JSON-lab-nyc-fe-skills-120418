355233faca29f5258bba146480cf68d7d87cfaeb
joseph Chun <jjunior.chun@gmail.com>
2019-02-01 01:18:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,19 +6,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
   
-const title = document.getElementById("title")
-const director = document.getElementById("director")
-const genre = document.getElementById("genre")
-const filmRating = document.getElementById("filmRating")
 
-const poster = document.getElementById("poster")
-const description = document.getElementById("description")
-const audienceScore = document.getElementById("audienceScore")
-const listSelection = document.getElementById("listSelection")
-const cast = document.getElementById("cast")
-const actor = document.getElementById("actor")
-
-console.log(movies.Titanic.title)
 
 //1 
 const titanicButton = document.getElementById("Titanic")
@@ -29,6 +17,11 @@ const terminatorButton = document.getElementById("Terminator 2")
 const changeMovie = (movie) => {
   const titleElement = document.getElementById("title")
   titleElement.innerHTML = movies[movie].title
+  const directorElement = document.getElementById("director")
+  directorElement.innerHTML = movies[movie].director
+  const genreElement = document.getElementById("genre")
+  genreElement = document.getElementById("genre")
+  
 }
 
 titanicButton.addEventListener("click", () => changeMovie("Titanic"))
